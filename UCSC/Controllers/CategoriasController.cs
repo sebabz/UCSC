2cb8ca1a914879fe722401c8a36d4de11b1469d1
sebabz <sebastianprovoste@gmail.com>
2021-06-17 23:13:54 -0400

    Categoria SemiListo EPP Crear

---
+++ UCSC/Controllers/CategoriasController.cs
@@ -0,0 +1,67 @@
+﻿using UCSC.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UCSC.Controllers
+{
+    public class CategoriasController : Controller
+    {
+        private gestion_eppEntities db = new gestion_eppEntities();
+        // GET: Categorias
+        public ActionResult Index()
+        {
+            var categorias = db.Categoria.ToList();
+            return View(categorias);
+                     
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Categoria categoria)
+        {
+            db.Categoria.Add(categoria);
+            db.SaveChanges();
+            return View();
+        }
+
+        public ActionResult Edit(int? id)
+        {
+            Categoria categoria = db.Categoria.Find(id);
+            return PartialView("_Edit", categoria);
+        }
+        [HttpPost]
+        public ActionResult Edit(Categoria categoria)
+        {
+            db.Entry(categoria).State = EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult Delete(int? id)
+        {
+            if (id != null)
+            {
+                var categoria = db.Categoria.Find(id);
+                return PartialView("_Delete", categoria);
+            }
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            var categoria = db.Categoria.Find(id);
+            db.Categoria.Remove(categoria);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+
+    }
+}
---
+++ UCSC/Controllers/EPPController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UCSC.Models;
+
+namespace UCSC.Controllers
+{
+    public class EPPController : Controller
+    {
+        private gestion_eppEntities db = new gestion_eppEntities();
+
+
+        // GET: EPP
+        public ActionResult Index()
+        {
+
+            var EPP = db.EPP.ToList();
+            return View(EPP);
+        }
+
+
+        public ActionResult Create()
+        {
+            ViewBag.categoria = new SelectList(db.Categoria, "id_categoria", "nombre");
+            ViewBag.usuario = new SelectList(db.Usuario, "id_usuario", "nombre");
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(EPP epp)
+        {
+            try
+            {
+                db.EPP.Add(epp);
+                db.SaveChanges();
+                return Json("");
+            }
+
+            catch (Exception)
+            {
+                return Json("Ha ocurrido un error, intentelo mas tarde.");
+            }
+        }
+
+        public ActionResult Edit(int id)
+        {
+           
+            var epp = db.EPP.Find(id);
+            
+            ViewBag.proveedor = new SelectList(db.Usuario, "id_usuario", "nombre", epp.id_user);
+            return PartialView("_Edit", epp);
+        }
+        [HttpPost]
+        public ActionResult Edit(EPP epp)
+        {
+
+            db.Entry(epp).State = EntityState.Modified;
+            db.SaveChanges();
+            return Json("");
+        }
+
+
+
+
+
+    }
+}
